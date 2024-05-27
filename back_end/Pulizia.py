86eb36d739ede92dc01654b45a24d16ea3ac44bd
Krioos <alessionatale2k@gmail.com>
2024-05-27 09:35:44 +0200

    Pulizia ristoranti sotto 10 recensioni

---
+++ back_end/Pulizia.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+path = input("Mettere il path del csv da pulire")
+df = pd.read_csv(path)
+df.rename(columns={'Unnamed: 0': 'index'}, inplace=True)
+df_filtrato = df[df['total_reviews_count'] >= 10]
+df_filtrato.to_csv('Dataset_ancora_più_pulito.csv', index=False)
+print(df_filtrato.head())
+print(df)
