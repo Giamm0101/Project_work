f82c37e0712016aa02ca17fec5102cd3dfcf0970
Pomasamuele <p.samuele.info@gmail.com>
2024-05-28 15:48:56 +0200

    card ristorante

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 
 
 app = Flask(__name__)
@@ -17,6 +17,22 @@ def chi_siamo():
 @app.route('/contatti')
 def contatti():
     return render_template('contatti.html')
+cards = [{'id': i, 'title': f'Card {i}', 'content': f'Content of card {i}'} for i in range(1, 101)]
+
+# Numero di card per pagina
+CARDS_PER_PAGE = 10
+
+@app.route('/api/cards')
+def get_cards():
+    page = int(request.args.get('page', 1))
+    start = (page - 1) * CARDS_PER_PAGE
+    end = start + CARDS_PER_PAGE
+    paginated_cards = cards[start:end]
+    return jsonify(paginated_cards)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
