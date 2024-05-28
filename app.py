7235eec2f8b20b7004c7bb13e0b6084aebcd92af
Giamm0101 <giammarcomuscedere98@gmail.com>
2024-05-28 10:11:33 +0200

    chi-siamo e contatti

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route('/chi_siamo')
+def chi_siamo():
+    return render_template('chi_siamo_html')
+
+@app.route('/contatti')
+def contatti():
+    return render_template('contatti.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
