6ae2b27fe482fd95648c34735f1a5d9771bc684c
Giamm0101 <giammarcomuscedere98@gmail.com>
2024-05-29 10:31:47 +0200

    ristoranti

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
 from flask import Flask, request, jsonify, render_template
-
+import mysql.connector
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
+
+def create_db_connection():
+    db_config = {
+        'host': app.config['MYSQL_HOST'],
+        'user': app.config['MYSQL_USER'],
+        'password': app.config['MYSQL_PASSWORD'],
+        'database': app.config['MYSQL_DB']
+    }
+    return mysql.connector.connect(**db_config)
+
+def execute_query(query, params=None):
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
+
 @app.route('/')  # route di default con slash
 def home():
     return render_template("home.html")
 
 @app.route('/ristoranti')
 def ristoranti():
-    return render_template("ristoranti.html")
+    ristoranti = execute_query('SELECT * FROM book')
+    return render_template("ristoranti.html", ristoranti=ristoranti)
 
 @app.route('/chi_siamo')
 def chi_siamo():
