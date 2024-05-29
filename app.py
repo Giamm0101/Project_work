ba2f56eb681174b53cb5de75cb3be31c3b2bcca8
Giamm0101 <giammarcomuscedere98@gmail.com>
2024-05-29 10:38:28 +0200

    pulite route

--- app.py
+++ app.py
@@ -41,18 +41,6 @@ def chi_siamo():
 @app.route('/contatti')
 def contatti():
     return render_template('contatti.html')
-cards = [{'id': i, 'title': f'Card {i}', 'content': f'Content of card {i}'} for i in range(1, 101)]
-
-# Numero di card per pagina
-CARDS_PER_PAGE = 10
-
-@app.route('/api/cards')
-def get_cards():
-    page = int(request.args.get('page', 1))
-    start = (page - 1) * CARDS_PER_PAGE
-    end = start + CARDS_PER_PAGE
-    paginated_cards = cards[start:end]
-    return jsonify(paginated_cards)
 
 @app.route('/')
 def index():
