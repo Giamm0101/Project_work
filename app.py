6be487e5d489cf5e81ab73ed860dc05e8e33a392
Giamm0101 <giammarcomuscedere98@gmail.com>
2024-05-30 12:01:01 +0200

    ricerca ristoranti

--- app.py
+++ app.py
@@ -29,17 +29,51 @@ def execute_query(query, params=None):
 def home():
     return render_template("home.html")
 
-@app.route('/ristoranti')
+@app.route('/ristoranti', methods=['GET', 'POST'])
 def ristoranti():
-    nr = execute_query('SELECT COUNT(*) as n FROM restaurant ')
-    nr = nr[0]['n']
-    np =request.args.get('page')
-    if np == None:
-        np =0
+    if request.method == 'POST':
+
+        city = request.form['city']
+        diet = request.args.get('diet')
+
+        # Conta il numero totale di ristoranti
+        query = """SELECT COUNT(*) as n FROM restaurant AS r JOIN risto_diet AS rd ON r.restaurant_id=rd.restaurant_id JOIN special_diet as sp 
+        ON rd.special_diet_id=sp.special_diet_id JOIN location AS l ON r.location_id=l.location_id WHERE l.city = %s"""
+        params = [city]
+        if diet:
+            query += " AND rd.special_diet_id = %s"
+            params.append(diet)
+
+        nr = execute_query(query, tuple(params))
+        nr = nr[0]['n']
+        
+        # Gestione della paginazione
+        np = request.args.get('page')
+        if np is None:
+            np = 0
+        else:
+            np = int(np)
+        
+        # Recupera i ristoranti per la pagina corrente
+        query = """SELECT * FROM restaurant AS r JOIN risto_diet AS rd ON r.restaurant_id=rd.restaurant_id JOIN special_diet as sp 
+        ON rd.special_diet_id=sp.special_diet_id JOIN location AS l ON r.location_id=l.location_id WHERE l.city = %s GROUP BY r.restaurant_id"""
+        if diet:
+            query += " AND rd.special_diet_id = %s"
+        query += " LIMIT 15 OFFSET %s"
+        params.append(np * 15)
+        
+        ristoranti = execute_query(query, tuple(params))
+        print(ristoranti[0])
+        
+        return render_template("ristoranti.html", ristoranti=ristoranti, page=np, max_page=(nr // 15))
     else:
-        np = int(np)
-    ristoranti = execute_query('SELECT * FROM restaurant LIMIT 15 offset %s', (np*15,))
-    return render_template("ristoranti.html", ristoranti=ristoranti, page = np, max_page = nr//15)
+        # Handle the initial GET request (no city provided)
+        city = None  # Or set a default city
+        ristoranti = []  # Empty list of restaurants
+        page = 0
+        max_page = 0  # Set appropriate values for pagination
+        return render_template("ristoranti.html", ristoranti=ristoranti, page=page, max_page=max_page)
+
 
 @app.route('/ristoranti/<country>')
 def ristoranti_country(country):
